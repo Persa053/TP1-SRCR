d0f9385c47d97bad362e73ef6b2f9461180215b9
Persa53 <50404710+Persa053@users.noreply.github.com>
2021-03-30 23:27:04 +0100

    Add files via upload

---
+++ Covid.pl
@@ -0,0 +1,450 @@
+%---------------------------------------------------------------------
+% SIST. REPR. CONHECIMENTO E RACIOCINIO - MiEI/3ºAno
+
+%---------------------------------------------------------------------
+% TRABALHO PRÁTICO: PARTE 1    2020/2021
+
+%---------------------------------------------------------------------
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
+:- set_prolog_flag( unknown,fail ).
+
+:- op( 900,xfy,'::' ).
+:- dynamic utente/9.
+:- dynamic centrosaude/5.
+:- dynamic staff/4.
+:- dynamic vacinacao/7.
+:- dynamic medico/5.
+:- dynamic enfermeiro/5.
+:- dynamic ano/1.
+
+% Ano atual
+ano(2021).
+
+%solucoes(T,Q,S) :- findall(T,Q,S).
+
+solucoes(X, XS, R) :- XS, assert(tmp(X)), fail.
+solucoes(X, XS, R) :- solucoesAux([], R).
+
+solucoesAux(L, R) :- retract(tmp(X)), !, solucoesAux([X|L], R).
+solucoesAux(R, R).
+
+
+
+evolucao( Termo ) :-
+  solucoes( Invariante,+Termo::Invariante,Lista ),
+    insercao( Termo ),
+      teste( Lista ).
+
+insercao( Termo) :-
+        assert( Termo ).
+insercao( Termo) :-
+        retract( Termo ),!,fail.
+
+
+
+
+
+
+
+%---------------------------------------------------------------------
+%---------------------------------------------------------------------
+%----------------------- Base de Conhecimento ------------------------
+%---------------------------------------------------------------------
+%---------------------------------------------------------------------
+
+
+
+%---------------------------------------------------------------------
+%-------- Utente -----------------------------------------------------
+%---------------------------------------------------------------------
+%Utente: #Idutente, Nº Segurança_Social, Nome, Data_Nasc, Email, Telefone, Morada, Profissão, [Doenças_Crónicas], #CentroSaúde ↝ { 𝕍, 𝔽}
+
+utente(1, 087462728, 'Jose Oliveira',1965, 'joseoliv@gmail.com', 917263549, braga, engenheiro, [colestrol]).
+utente(2, 528102846, 'Diogo Espirito Santo',1956, 'diant@gmail.com', 936489367, coimbra, medico, []).
+utente(3, 658746027, 'Afonso Castro', 1997, 'fonso@gmail.com', 927362918,braga, empresario, [hipertensao]).
+utente(4, 129837465, 'Duarte Catalao', 1986, 'dudu@gmail.com', 918374526,lisboa, arquiteto,[osteoporose]).
+utente(5, 092183746, 'Lara Vilhena', 1996, 'laravilhena12@gmail.com', 9387362907, porto, modelo, []).
+utente(6, 394876938, 'Joao Silva', 1994, 'jonysilva@gmail.com', 912653829, guimaraes, futebolista, []).
+utente(7, 984735620, 'Maria Marques', 2000, 'marymar@gmail.com', 939273829, porto, estudante, []).
+utente(8, 093847568, 'Sofia Alves', 1998, 'sofsalves4@gmail.com', 918273648,lisboa, enfermeira, []).
+utente(9, 498327462, 'Andre Pereira',1995, 'andregoper56@gmail.com', 928756401, porto, cozinheiro, [asma]).
+utente(10, 834768723, 'Rui Vilela', 2001, 'ruivilela@gmail.com', 935468937, barcelos, estudante, []).
+utente(11, 092134546, 'Veronica Pontes', 2000, 'veronica63@gmail.com', 918273648, guimaraes, estudante, []).
+utente(12, 398475394, 'Francisca Avila',1954, 'franciscavila@gmail.com', 938476032, braga, medica, [alzheimer]).
+utente(13, 298374563, 'Pedro Gouveia', 1967, 'pedrogouveia@gmail.com', 928354082, lisboa, professor, []).
+utente(14, 138473578, 'Sofia Alves', 1972, 'sofsalves4@gmail.com', 937485926, fafe, engenheira, [diabetes]).
+utente(15, 578328474, 'Margarida Maia', 1988, 'margarida_m@gmail.com', 923457366, aveiro, terapeuta, [asma]).
+utente(16, 873645634, 'Ana Lopes', 1958, 'analopes@gmail.com', 912375648, braga, professora, [diabetes,rinite]).
+utente(17, 337462863, 'Dalila Pavao', 1965, 'dalilapavao@gmail.com', 937593746, porto, engenheira, [parkinson]).
+utente(18, 163874673, 'Manuela Rodrigues', 1977, 'manuelarodrigues@gmail.com', 912847562, guimaraes, policia, []).
+utente(19, 098475833, 'Jose Soares', 1989, 'josesoaresvilela@gmail.com', 937264839, faro, padeiro, []).
+utente(20, 763487435, 'Luis Veloso', 1978, 'luisveloso@gmail.com', 927465839, aveiro, marinheiro, []).
+
+
+
+%---------------------------------------------------------------------
+%-------- CentroSaude ------------------------------------------------
+%---------------------------------------------------------------------
+%centro_saúde: #Idcentro, Nome, Morada, Telefone, Email ↝ { 𝕍, 𝔽}
+
+centrosaude(1, 'Centro de Saúde de Braga', 'Largo Paulo Orósio, 4700-031 Braga', 253928647, 'csbraga@gmail.com').
+centrosaude(2, 'USF Bracara Augusta', 'Praça Gen. Humberto Delgado 47, 4715-213 Braga', 253964876, 'bracaraaugusta@gmail.com').
+centrosaude(3, 'USF Sanus Caranda', 'R. André Soares 25, 4715-213 Braga', 253201530, 'usfcaranda@gmail.com').
+centrosaude(4, 'Centro de Saúde de Guimarães', 'R. Francisco Santos Guimarães, 4810-225 Guimarães', 253519923, 'csguimaraes@gmail.com').
+centrosaude(5, 'Centro de Saude Amorosa', 'R. José Pinto Rodrigues 16, Guimarães, 4810-225 Guimarães', 253421340, 'csamorosa@gmail.com').
+centrosaude(6, 'Unidade de Saúde Familiar Cruz de Celas', 'Av. Dom Afonso Henriques 141, 3000-011 Coimbra', 239488261, 'usfcelas@gmail.com').
+centrosaude(7, 'USCP Fernão de Magalhães', 'Av. Fernão de Magalhães 620, 3000-174 Coimbra', 239856110, 'uscpfernaomagalhaes@gmail.com').
+centrosaude(8, 'Centro de Saúde Norton de Matos', '3030-790 Coimbra', 239794110, 'csnorton@gmail.com').
+centrosaude(9, 'Centro de Saúde da Penha de França', 'R. Luís Pinto Moitinho 5, Lisboa', 218164100, 'cspenhadefranca@gmail.com').
+centrosaude(10, 'Centro de Saúde de Sete Rios', 'R. São Domingos de Benfica 20, Lisboa', 2172171800, 'csseterios@gmail.com').
+centrosaude(11, 'Centro de Saúde da Lapa', 'R. São Domingos de Benfica 20, Lisboa', 217211800, 'cslapa@gmail.com').
+centrosaude(12, 'Centro Saúde do Lumiar', '5°/6°, 144, Alameda das Linhas de Torres 243, Lisboa', 217527110, 'cslumiar@gmail.com').
+centrosaude(13, 'USF Porto Centro', 'Rua de Santos Pousada 298, 4000-478 Porto', 225360060, 'usfporto@gmail.com').
+centrosaude(14, 'Centro de Saúde de Paranhos', '510, R. de Vale Formoso 466, Porto', 22 834 7355, 'csparanhos@gmail.com').
+centrosaude(15, 'Centro de Saúde do Bonfim', 'R. do Barão de Nova Sintra 244, 4300-367 Porto', 225898560, 'csbonfim@gmail.com').
+centrosaude(16, 'USF Porto Centro', 'Rua de Santos Pousada 298, 4000-478 Porto', 225360060, 'usfporto@gmail.com').
+centrosaude(17, 'Centro de Saúde de Faro', 'Urbanização Hortas das Figuras, 8009-003 Faro', 289830300, 'csfaro@gmail.com').
+centrosaude(18, 'USF Novo Cuidar', 'R. José Ribeiro Vieira de Castro 125, 4820-273 Fafe', 253490863, 'usffafe@gmail.com').
+centrosaude(19, 'Centro de Saúde de Aveiro', 'Viela da Fonte dos Amores 57A, 3810-164 Aveiro', 234891170, 'csaveiro@gmail.com').
+centrosaude(20, 'Centro de Saúde de Barcelos', 'R. Dr. Abel Varzim, 4750-253 Barcelos', 253808300, 'csbarcelos@gmail.com').
+
+
+%---------------------------------------------------------------------
+%-------- CentroSaude ------------------------------------------------
+%---------------------------------------------------------------------
+%staff: #Cstaff, #Idcentro, Nome, email ↝ { 𝕍, 𝔽 }
+
+staff(1, 1, 'Monica Sintra','monicas@gmail.com').
+staff(2, 1, 'Cristiano Alves', 'cristianoalves@gmail.com').
+staff(3, 1, 'Jorge Pires', 'jorgeemanuel@gmail.com').
+staff(4, 2, 'Antonio Goncalves', 'antoniogoncalves@gmail.com').
+staff(5, 2, 'Marco Barbosa', 'marquitxi@gmail.com').
+staff(6, 2, 'Sara Oliveira', 'sarinha@gmail.com').
+staff(7, 3, 'Joana Marques', 'joanamarques@gmail.com').
+staff(8, 3, 'Emanuel Barros', 'emanuelbarros@gmail.com').
+staff(9, 4, 'Paulo Rodrigues', 'paulinho@gmail.com').
+staff(10, 4, 'Tiago Silva', 'tiagosilva@gmail.com').
+staff(11, 5, 'Anabela Alves', 'anabelalves@gmail.com').
+staff(12, 5, 'Vitor Moutinho', 'vitor39@gmail.com').
+staff(13, 6, 'Hugo Martins', 'hugo@gmail.com').
+staff(14, 6, 'Rita Noronha', 'rita@gmail.com').
+staff(15, 7, 'Diogo Alves', 'diogo57@gmail.com').
+staff(16, 7, 'Mateus Jordao', 'mateujord@gmail.com').
+staff(17, 8, 'Manuel Ferreira', 'manuelferr@gmail.com').
+staff(18, 8, 'Mariana Vieira', 'marianavieira@gmail.com').
+staff(19, 9, 'Lucas Barbosa', 'lucasbarb@gmail.com').
+staff(20, 9, 'Salvador Campos', 'salvadorca@gmail.com').
+staff(21, 10, 'Guilherme Falcao', 'guilhermefalcao@gmail.com').
+staff(22, 10, 'Sofia Mendes', 'sofiamendes@gmail.com').
+staff(23, 10, 'Henrique Paz', 'henrique@gmail.com').
+staff(24, 11, 'Micaela Carreira', 'micaela@gmail.com').
+staff(25, 12, 'Ana Rodrigues', 'anarodri@gmail.com').
+staff(26, 12, 'Maria Miguel Lopes', 'mariamig@gmail.com').
+staff(27, 13, 'Filomena Cardoso', 'cardoso@gmail.com').
+staff(28, 14, 'Miguel Figueiredo', 'miguelfigueiredo@gmail.com').
+staff(29, 14, 'Tomas Quintas', 'tomasquintas@gmail.com').
+staff(30, 15, 'Ana Jorge', 'anajor@gmail.com').
+staff(31, 15, 'Jose Pedro Quintas', 'josepedro@gmail.com').
+staff(32, 16, 'Rita Negroes', 'ritanegroes@gmail.com').
+staff(33, 17, 'Jorge Borges', 'jorgeborges@gmail.com').
+staff(34, 18, 'Fernando Santos', 'fernandosantos@gmail.com').
+staff(35, 19, 'Paulo Filipe', 'paulpfilipe@gmail.com').
+staff(36, 20, 'Filipa Trindade', 'filipatrindade@gmail.com').
+
+%---------------------------------------------------------------------
+%-------- Vacinação --------------------------------------------------
+%---------------------------------------------------------------------
+%vacinação_Covid: #Staf, #utente, Dia, Mes,Ano, Vacina, Toma↝ { 𝕍, 𝔽 }
+
+vacinacao(8,12,23,03,2021, pfizer, 0).
+vacinacao(24,8,01,04,2021, pfizer, 1).
+vacinacao(13,2,20,04,2021, pfizer, 0).
+vacinacao(1,1,02,05,2021, astraZeneca, 2).
+vacinacao(6,16,31,05,2021, pfizer, 1).
+vacinacao(25,13,29,06,2021, pfizer, 1).
+vacinacao(34,14,20,06,2021, astraZeneca, 0).
+vacinacao(31,17,19,07,2021, pfizer, 1).
+vacinacao(10,18,21,07,2021, astraZeneca, 2).
+vacinacao(35,20,13,08,2021, pfizer, 0).
+vacinacao(4,3,31,09,2021, pfizer, 0).
+vacinacao(36,10,05,09,2021, pfizer, 0).
+vacinacao(26,5,09,10,2021, pfizer, 1).
+vacinacao(33,19,21,11,2021, astraZeneca, 0).
+vacinacao(12,11,26,12,2021, astraZeneca, 1).
+vacinacao(32,9,30,01,2021, pfizer, 1).
+vacinacao(20,4,18,02,2021, pfizer, 1).
+vacinacao(28,7,25,01,2022, astraZeneca, 1).
+vacinacao(9,6,13,02,2022, astraZeneca, 2).
+vacinacao(35,15,07,01,2022, pfizer, 2).
+
+%---------------------------------------------------------------------
+%-------- MedicoFamilia ----------------------------------------------
+%---------------------------------------------------------------------
+% medicoFamilia: #IdMedico,Nome,Idade,Género,#CentroSaude -> {V,F}
+
+
+medico(1,'Ester Domingues',47,'F',1).
+medico(2,'Manuel Castro',29,'M',2).
+medico(3,'Emanuel Anjo',38,'M',3).
+medico(4,'David Ferreira',43,'M',4).
+medico(5,'Pedro Melo',40,'M',5).
+medico(6,'Ana Rita Nobre',38,'F',6).
+medico(7,'Fábio Gomes',56,'M',7).
+medico(8,'Renato Mendes',34,'M',8).
+medico(9,'Jose Cracel',61,'M',9).
+medico(10,'Catia Gomes',28,'F',10).
+medico(11,'Luisa Veloso',33,'F',11).
+medico(12,'Marta Pereira',47,'F',12).
+medico(13,'Leonor Gonçalves',39,'F',13).
+medico(14,'André Fernandes',30,'M',14).
+medico(15,'Duarte Pinto',55,'M',15).
+medico(16,'Maria Santos',40,'F',16).
+medico(17,'Marco Marques',46,'M',17).
+medico(18,'Isabel Arantes',58,'F',18).
+medico(19,'Duarte Teixeira',44,'M',19).
+medico(20,'Sara Carvalho',41,'F',20).
+
+
+
+
+
+%---------------------------------------------------------------------
+%-------- Enfermeiro -------------------------------------------------
+%---------------------------------------------------------------------
+% enfermeiro: #IdEnfermeiro,Nome,Idade,Género,#CentroSaude -> {V,F}
+
+enfermeiro(1,'Márcia Araujo',27,'F',1).
+enfermeiro(2,'Luzia Gomes',41,'F',2).
+enfermeiro(3,'Mafalda Araujo',50,'F',3).
+enfermeiro(4,'Henrique Megre',36,'M',4).
+enfermeiro(5,'Daniela Sousa',53,'F',5).
+enfermeiro(6,'Sara Maia',25,'F',6).
+enfermeiro(7,'Beatriz Silva',46,'F',7).
+enfermeiro(8,'Gabriela Simoes',50,'F',8).
+enfermeiro(9,'Alexandre Castro',30,'M',9).
+enfermeiro(10,'Ines Pitrez',55,'F',10).
+enfermeiro(11,'Eduarda Martins',26,'F',11).
+enfermeiro(12,'Rui Lourenço',37,'M',12).
+enfermeiro(13,'Henrique Faria',54,'M',13).
+enfermeiro(14,'Eduarda Azevedo',38,'F',14).
+enfermeiro(15,'Jorge Handel',44,'M',15).
+enfermeiro(16,'Marta Enes',47,'F',16).
+enfermeiro(17,'Daniela Pinto',52,'F',17).
+enfermeiro(18,'Leonor Taborda',48,'F',18).
+enfermeiro(19,'Janico Abreu',34,'M',19).
+enfermeiro(20,'Diana Vieira',39,'F',20).
+
+
+
+
+
+
+
+
+
+% fase1Vacincacao: #IdEnfermeiro,Nome,Idade,Género,#CentroSaude -> {V,F}
+%1a fase 1 Vacinacao
+
+
+faseVacinacao(1, utente(ID,_,_,Idade,_,_,_,_,_)) :- ano(Y), R is (Y-Idade),  R > 85.
+faseVacinacao(1, utente(ID,_,_,_,_,_,_,medico,_)).
+faseVacinacao(1, utente(ID,_,_,_,_,_,_,medica,_)).
+faseVacinacao(1, utente(ID,_,_,_,_,_,_,enfermeiro,_)).
+faseVacinacao(1, utente(ID,_,_,_,_,_,_,enfermeira,_)).
+faseVacinacao(1, utente(ID,_,_,_,_,_,_,_,Doencas)) :- length(Doencas, R), R > 0.
+
+listaFase(1,Us) :- findall(1, faseVacinacao(1,U), Us).
+
+
+
+% fase2Vacincacao: #IdEnfermeiro,Nome,Idade,Género,#CentroSaude -> {V,F}
+%1a fase 2 Vacinacao
+
+faseVacinacao(2, utente(ID,_,_,_,_,_,_,_,Doencas), ID).
+
+
+% fase1Vacincacao: #IdEnfermeiro,Nome,Idade,Género,#CentroSaude -> {V,F}
+%1a fase 1 Vacinacao
+
+
+
+%---------------------------------------------------------------------
+% Identificar utentes/centrosaude/staff por critérios de seleção
+
+%--------- Utentes
+
+
+%utente(20, 763487435, 'Luis Veloso', 07-08-1978, 'luisveloso@gmail.com', 927465839, aveiro, marinheiro, []).
+%Utente: #Idutente, Nº Segurança_Social, Nome, Data_Nasc, Email, Telefone, Morada, Profissão, [Doenças_Crónicas], #CentroSaúde ↝ { 𝕍, 𝔽}
+utenteId(Id, R) :-
+        solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+
+utenteNrSs(Ss, R) :-
+        solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+
+utenteNome(N, R) :-
+        solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+
+utenteDataNascimento(D, R) :-
+        solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+
+utenteEmail(E, R) :-
+        solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+
+utenteTelefone(Tlf, R) :-
+        solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+
+utenteMorada(M, R) :-
+        solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+
+utenteProfissao(P, R) :-
+        solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+%?????????????????????
+%utenteDoencas(DC, R) :-
+%       solucoes(utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), utente(Id,Ss,N,Dt,E,Tlf,M,P,DC), R).
+
+
+
+
+
+
+
+
+
+%--------- CentroSaude
+
+
+%centro_saúde: #Idcentro, Nome, Morada, Telefone, Email ↝ { 𝕍, 𝔽}
+%centrosaude(1, 'Centro de Saúde de Braga', 'Largo Paulo Orósio, 4700-031 Braga', 253928647, 'csbraga@gmail.com').
+
+centroSaudeId(Id, R) :-
+        solucoes(centrosaude(Id,N,M,Tlf,E), centrosaude(Id,N,M,Tlf,E), R).
+
+centroSaudeNome(N, R) :-
+        solucoes(centrosaude(Id,N,M,Tlf,E), centrosaude(Id,N,M,Tlf,E), R).
+
+centroSaudeMorada(M, R) :-
+        solucoes(centrosaude(Id,N,M,Tlf,E), centrosaude(Id,N,M,Tlf,E), R).
+
+centroSaudeTelefone(Tlf, R) :-
+        solucoes(centrosaude(Id,N,M,Tlf,E), centrosaude(Id,N,M,Tlf,E), R).
+
+centroSaudeEmail(E, R) :-
+        solucoes(centrosaude(Id,N,M,Tlf,E), centrosaude(Id,N,M,Tlf,E), R).
+
+
+%--------- Staff
+
+%staff: #Cstaff, #Idcentro, Nome, email ↝ { 𝕍, 𝔽 }
+%staff(1, 1, 'Monica Sintra','monicas@gmail.com').
+
+staffId(Id, R) :-
+        solucoes(staff(Id,Cs,N,E), staff(Id,Cs,N,E), R).
+
+staffCentroSaude(Cs, R) :-
+        solucoes(staff(Id,Cs,N,E), staff(Id,Cs,N,E), R).
+
+staffNome(N, R) :-
+        solucoes(staff(Id,Cs,N,E), staff(Id,Cs,N,E), R).
+
+staffEmail(E, R) :-
+        solucoes(staff(Id,Cs,N,E), staff(Id,Cs,N,E), R).
+
+
+
+%--------- MedicoFamilia
+
+% medicoFamilia: #IdMedico,Nome,Idade,Género,#CentroSaude -> {V,F}
+%medico(1,'Ester Domingues',47,'F',1).
+
+
+medicoId(Id, R) :-
+        solucoes(medico(Id,N,I,G,Cs), medico(Id,N,I,G,Cs), R).
+
+medicoNome(N, R) :-
+        solucoes(medico(Id,N,I,G,Cs), medico(Id,N,I,G,Cs), R).
+
+medicoIdade(I, R) :-
+        solucoes(medico(Id,N,I,G,Cs), medico(Id,N,I,G,Cs), R).
+
+medicoGenero(G, R) :-
+        solucoes(medico(Id,N,I,G,Cs), medico(Id,N,I,G,Cs), R).
+
+medicoCentroSaude(Cs, R) :-
+        solucoes(medico(Id,N,I,G,Cs), medico(Id,N,I,G,Cs), R).
+
+
+%--------- Enfermeiro
+
+% enfermeiro: #IdEnfermeiro,Nome,Idade,Género,#CentroSaude -> {V,F}
+% enfermeiro(1,'Márcia Araujo',27,'F',1).
+
+enfermeiroID(Id, R) :- solucoes(enfermeiro(Id,N,I,G,Cs), enfermeiro(Id,N,I,G,Cs), R).
+
+enfermeiroNome(N, R) :- solucoes(enfermeiro(Id,N,I,G,Cs), enfermeiro(Id,N,I,G,Cs), R).
+
+enfermeiroIdade(I, R) :- solucoes(enfermeiro(Id,N,I,G,Cs), enfermeiro(Id,N,I,G,Cs), R).
+
+enfermeiroGenero(G, R) :- solucoes(enfermeiro(Id,N,I,G,Cs), enfermeiro(Id,N,I,G,Cs), R).
+
+enfermeiroCservico(Cs, R) :- solucoes(enfermeiro(Id,N,I,G,Cs), enfermeiro(Id,N,I,G,Cs), R).
+
+
+
+
+
+
+
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Faz a insercao de conhecimento
+% Extensao predicado que permite a evolucao conhecimento: Termo - {V,F}
+
+
+
+
+
+
+
+
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Faz a removoção de conhecimento
+% Extensão predicado que permite a involucao conhecimento: Termo - {V,F}
+
+
+%involucao( Termo ) :-
+%        solucoes( Invariante, -Termo::Invariante, Lista ),
+%         remocao( Termo ),
+%        teste( Lista ).
+
+%remocao( Termo ):-
+%        retract( Termo ).
+%remocao( Termo ):-
+%        assert( Termo ),!,fail.
+
+%Permitir a definição de fases de vacinação, definindo critérios de inclusão de utentes nas diferentes fases (e.g., doenças crónicas, idade, profissão);
+
+%---------------------------------------------------------------------
+%identificar pessoas não vacinadas;
+
+
+
+
+
+%desenvolver um sistema de inferencia capaz de implementar os mecanismos de raciocinio inerentes a estes sistemas;
+
+
+
+
+% ---------------------------------------------------------------------
+% ---------------------------------------------------------------------
+% --------------------------- Auxiliares ------------------------------
+% ---------------------------------------------------------------------
